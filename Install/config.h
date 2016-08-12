eedeec26e11184a490a77dd7e1de314a9252abb7
praveennr <pravindran@dal.ca>
2016-08-12 13:13:37 -0300

    Changing

---
+++ Install/config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the compiler supports basic C++11 syntax */
+#define HAVE_CXX11 1
+
+/* Name of package */
+#define PACKAGE "pmerge"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "pmerge"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "pmerge 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "pmerge"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
